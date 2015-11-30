e69a2f343647876b4c883193ff55aea05504efaf
Joshua Guerra <joshua@allianceconsults.net>
2015-11-30 17:38:52 -0500

    complete dummy output

--- printAlpha/printAlpha.cpp
+++ printAlpha/printAlpha.cpp
@@ -16,12 +16,31 @@ void main() {
 	printTitle();
 
 	////////	printAlpha()			////////
-	////////	printAlpha('T')"		////////
-	////////	printAlpha('C', 'W')"		////////
+	cout
+		<< "Calling \"printAlpha()\"\n"
+		<< "\tPrinting from A to Z, 5 characters per line:\n\n";
+	printAlpha();
+
+	////////	printAlpha('T')			////////
+	cout
+		<< "Calling \"printAlpha('T')\"\n"
+		<< "\tPrinting from T to Z, 5 characters per line:\n\n";
+	printAlpha('T');
+
+	////////	printAlpha('C', 'W')		////////
+	cout
+		<< "Calling \"printAlpha('C', 'W')\"\n"
+		<< "\tPrinting from C to W, 5 characters per line:\n\n";
+	printAlpha('C', 'W');
+
 	////////	printAlpha('D', 'Y', 3)		////////
+	cout
+		<< "Calling \"printAlpha('D', 'Y', 3)\"\n"
+		<< "\tPrinting from D to Y, 3 characters per line:\n\n";
+	printAlpha('D', 'Y', 3);
+
+	system("pause");
 }
-
-
 
 void printTitle() {
 	cout
@@ -31,6 +50,9 @@ void printTitle() {
 }
 
 void printAlpha(char s, char e, int cpl) {
-	for (int l = s; l <= e; l++) 
-		cout << l << (l % cpl == cpl - 1 ? "\n" : "\t");
+	int count = 0;
+	
+	for (char l = s; l <= e; l++)
+		cout << l << (count++ % cpl == cpl - 1 ? "\n" : "\t");
+	cout << endl << endl;
 }
