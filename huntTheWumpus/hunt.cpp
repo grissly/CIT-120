153e23c9cebd96b39ba6d67625a6c9f99b682767
Joshua Guerra <joshua@allianceconsults.net>
2015-12-03 16:49:09 -0500

    final project initialized

---
+++ huntTheWumpus/hunt.cpp
@@ -0,0 +1,13 @@
+// Hunt the Wumpus - originally written in BASIC by Gregory Yob in 1973
+// File Name: huntTheWumpus
+// Programmer: Josh Guerra
+// Last Modified: Dec 2, 2015
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void main() {
+
+}
